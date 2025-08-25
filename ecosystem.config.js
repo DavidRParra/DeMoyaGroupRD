214475583f4c0eb58089214103da2fa8a4ecd3b2
David R. Parra <davidparra9727@gmail.com>
2025-08-25 11:52:43 -0400

    Configuracion de pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    // Configuración para el entorno de PRODUCCIÓN
+    name: "EddyLopez-prod", // Nombre del proceso en PM2
+    script: "npm",                    // Usamos npm para ejecutar el script
+    args: "run start",                // El comando "npm run start"
+    env: {
+      NODE_ENV: "production",
+      PORT: 3015,
+    },
+    // Este proceso debe ser usado después de ejecutar "npm run build"
+  }]
+};
